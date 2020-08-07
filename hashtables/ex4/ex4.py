566de8f79aa8a317e5446e3124a6144d4803cfda
Erik Rodriguez <eric19.21.03@gmail.com>
2020-08-07 10:55:16 -0700

    Adds exercise 4 code to hastable folder

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,30 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    # set cache to empty dictionary
+    cache = {}
+
+    # set result to list to catch pos / neg pair
+    result = []
+
+    # for loop through num of a
+    for num in a:
+        # remove symbol from num with absolute value method
+        new_num = abs(num)
+        # check if new_num in dictionary
+        if new_num in cache:
+            # then add one to cache[new_num]
+            cache[new_num] += 1
+        else:
+            # create a key cache[num] set to 1
+            cache[new_num] = 1
+
+    # for loop through pair of cache.items()
+    for pair in cache.items():
+        # check if pair[1] is equal to two
+        if pair[1] == 2:
+            # then append only the key to result list
+            result.append(pair[0])
 
     return result
 
