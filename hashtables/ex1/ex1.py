51844e0cd0e8aedff772f15233c601a27bf5a048
Erik Rodriguez <eric19.21.03@gmail.com>
2020-09-04 12:23:33 -0700

    Adds ex1 code to ex1.py file

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,29 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    # set result to empty list
+    result = []
 
+    # set cache to empty dictionary
+    cache = {}
+
+    # for loop through range length parameter
+    for i in range(length):
+        difference = limit - weights[i]
+        # check if difference is in cache with limit minus current index value
+        if difference in cache:
+            # append both indices to result list
+            result.append(i)
+            result.append(cache[difference])
+            # return result list with indices
+            return result
+        # else
+        else:
+            # store in cache for later use
+            cache[weights[i]] = i
+
+    # return none if nothing found
     return None
+
+
+print(get_indices_of_item_weights([4, 4], 2, 8))
